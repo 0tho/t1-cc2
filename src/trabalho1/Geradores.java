dcca1f3a88020a0f053d11a38285f557e00e0304
Matheus Silva <matheus.ars91@gmail.com>
2016-12-01 18:21:30 -0200

    Exemplos de estruturas reconhecidas que devem ser geradas

--- src/trabalho1/Geradores.java
+++ src/trabalho1/Geradores.java
@@ -5,5 +5,65 @@
 package trabalho1;
 
 public class Geradores {
+
+	public static String enterScope() {
+		return "";
+	}
+
+	public static String leaveEscope() {
+		return "";
+	}
+
+	public static String whileLoop() {
+		return "";
+	}
 	
+	public static String doWhileLoop() {
+		return "";
+	}
+	
+	public static String forLoop() {
+		return "";
+	}
+	
+	public static String foreachLoop() {
+		return "";
+	}
+
+	public static String attribution() {
+		return "";
+	}
+
+	public static String normalize() {
+		return "";
+	}
+
+	public static String method() {
+		return "";
+	}
+
+	public static String write() {
+		return "";
+	}
+
+	public static String read() {
+		return "";
+	}
+
+	public static String condition() {
+		return "";
+	}
+
+	public static String caseCondition(){
+		return "";
+	}
+
+	public static String beginAlgorithm(){
+		return "";
+	}
+
+	public static String endAlgorithm(){
+		return "";
+	}
+
 }

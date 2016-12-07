f658db89353940857a696e7616b6e6f16c065e3e
Matheus Silva <matheus.ars91@gmail.com>
2016-12-07 18:59:48 -0200

    Geração de código: DeclareConstante, DeclareVariavel, Dimensao, TipoReferencia

--- src/trabalho1/LaCGeneratorVisitor.java
+++ src/trabalho1/LaCGeneratorVisitor.java
@@ -18,6 +18,7 @@ public class LaCGeneratorVisitor extends LaParserBaseVisitor<String> {
   @Override
   public String visitPrograma(LaParser.ProgramaContext ctx) {
     Lac.geradorBuffer.println(tab() + "#include <stdio.h>");
+    Lac.geradorBuffer.println(tab() + "#include <stdlib.h>");
     Lac.geradorBuffer.println(tab() + "");
     Lac.geradorBuffer.println(tab() + "int main() {");
     tabCount++;
@@ -27,4 +28,36 @@ public class LaCGeneratorVisitor extends LaParserBaseVisitor<String> {
     Lac.geradorBuffer.println(tab() + "}");
     return null;
   }
+
+  @Override
+  public String visitDeclareConstante(LaParser.DeclareConstanteContext ctx) {
+    String simboloId = ctx.Ident().getText();
+    Lac.geradorBuffer.println("#define " + simboloId + (String) visit(ctx.valor_constante));
+    return null;
+  }
+
+  @Override
+  public String visitDeclareVariavel(LaParser.DeclareVariavelContext ctx) {
+    ArrayList<Simbolo> simbolos = (ArrayList<Simbolo>) visit(ctx.lista_variavel());
+
+    for( Simbolo s: simbolos ) {
+      Lac.geradorBuffer.println(tab() + s.getTipo() + " " + s.getNome() + ";");
+    }
+
+    return null;
+  }
+
+  @Override
+  public String visitDimensao(LaParser.DimensaoContext ctx) {
+    Lac.geradorBuffer.print("["+ (String) visit(ctx.exp_aritmetica()) +"]");
+    return null;
+  }
+
+  @Override
+  public String visitTipoReferencia(LaParser.TipoReferenciaContext ctx) {
+    Lac.geradorBuffer.println((String) visitChildren(ctx) + (ctx.Pointer() != null ? "":"* ")  + ctx.tipo_basico_identificador().getText());
+    return null;
+  }
+
+  
 }
